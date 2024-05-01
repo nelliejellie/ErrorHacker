80f27faa3655d10781a32459d7fb6d7a4bd2eedd
Timmydee <dahunsitimmy@gmail.com>
2024-05-01 09:34:38 +0100

    Context Implementation

---
+++ src/context/context.tsx
@@ -0,0 +1,11 @@
+import React, {ReactNode, createContext, useContext} from 'react';
+
+const MyContext = createContext(null);
+
+export const ContextProvider = ({children}: {children: ReactNode}) => {
+  // Data to be shared globally
+  
+  return <MyContext.Provider>{children}</MyContext.Provider>;
+};
+
+export const useMyContext = () => useContext(MyContext);
